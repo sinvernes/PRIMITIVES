ee53d23a8e3ef5aaa2ba6163729698c556780c6c
Stefanovich Sofia <sony.rowz@mail.ru>
2023-08-26 16:00:33 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amout = 18499;
+        int rubles = 20;
+        int bonus = amout / rubles;
+
+        System.out.println ("Бонусные мили:" + bonus);
+    }
+}
