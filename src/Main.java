8b775e3c308956168ec793a28d8d9d08a04d8ab7
Stefanovich Sofia <135492022+sinvernes@users.noreply.github.com>
2023-08-26 16:14:14 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int amout = 18499;
+        int amount = 18499;
         int rubles = 20;
         int bonus = amout / rubles;
 
